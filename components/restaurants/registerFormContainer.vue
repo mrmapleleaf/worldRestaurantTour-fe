c44f18acb058686f47a9367f3144807140cb6493
mrmapleleaf <kaple.de.96@gmail.com>
2024-12-29 22:40:35 +0900

    セレクトボックスの挙動を修正

--- components/restaurants/registerFormContainer.vue
+++ components/restaurants/registerFormContainer.vue
@@ -15,7 +15,17 @@
             (val) => lengthValidation(val, 50),
           ]"
         />
-        <q-select filled v-model="countryId" :options="options" label="国" />
+        <q-select
+          filled
+          v-model="countryId"
+          :options="countries"
+          option-label="name"
+          option-value="id"
+          emit-value
+          map-options
+          label="国"
+          :rules="[(val) => val || 'このフィールドは必須です']"
+        />
         <q-input
           filled
           v-model="url"
@@ -56,12 +66,15 @@
 </template>
 
 <script setup lang="ts">
+import type { AllCountriesIndexResponse, Countries } from '~/interfaces';
+
 const $q = useQuasar();
 const name = ref(null);
 const countryId = ref(null);
 const url = ref(null);
 const thoughts = ref(null);
-const options = ['Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'];
+
+let countries = ref([] as Array<Countries>);
 
 const onSubmit = () => {};
 
@@ -70,6 +83,21 @@ const onReset = () => {
   countryId.value = null;
   url.value = null;
   thoughts.value = null;
+};
+
+const getAllCountriesForOption = onMounted(() => {
+  getAllCountries();
+});
+
+const getAllCountries = async () => {
+  try {
+    const response = await $fetch<AllCountriesIndexResponse>(
+      'http://localhost:8080/country/allCountries'
+    );
+    countries.value = response.countries;
+  } catch (error) {
+    console.log(error);
+  }
 };
 </script>
 
