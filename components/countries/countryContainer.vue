5c0fce51659dea4cb93f630326a1f6ec9810af14
mrmapleleaf <kaple.de.96@gmail.com>
2025-01-11 17:54:04 +0900

    世界の国一覧をq-tableに変更

--- components/countries/countryContainer.vue
+++ components/countries/countryContainer.vue
@@ -9,32 +9,50 @@
           <p>表示する内容がありません</p>
         </template>
         <template v-else>
-          <div class="countryList q-pb-xl">
-            <q-markup-table bordered>
-              <thead>
-                <tr>
-                  <th>
-                    <span class="text-weight-bolder text-subtitle2">国名</span>
-                  </th>
-                  <th>
-                    <span class="text-weight-bolder text-subtitle2"
-                      >訪問経験</span
+          <div class="country-list q-pb-xl">
+            <div class="competed-countries-counter q-pb-lg">
+              <span class="q-pa-lg"
+                >訪問した国数：<span class="text-h4"
+                  >{{ completedCountryCount }} / {{ countries.length }}</span
+                >
+                ヵ国</span
+              >
+            </div>
+            <q-table
+              :rows="rows"
+              :columns="columns"
+              row-key="id"
+              bordered
+              :rows-per-page-options="[10, 25, 0]"
+            >
+              <template v-slot:header="props">
+                <q-tr :props="props">
+                  <q-th class="text-h1" v-for="column in columns">
+                    <span class="text-weight-bolder text-subtitle2">{{
+                      column.label
+                    }}</span></q-th
+                  >
+                </q-tr>
+              </template>
+              <template v-slot:body="props">
+                <q-tr :props="props" :key="`m_${props.row.id}`">
+                  <q-td>
+                    <NuxtLink
+                      class="link"
+                      :to="{
+                        name: 'restaurants-id',
+                        params: { id: props.row.id },
+                      }"
                     >
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="country in countries">
-                  <td>
-                    {{ country.name }}
-                  </td>
-                  <td v-if="country.completed">
-                    <a @click="onClickCountryName(country.id)">訪問済</a>
-                  </td>
-                  <td v-else>未訪問</td>
-                </tr>
-              </tbody>
-            </q-markup-table>
+                      {{ props.row.name }}
+                    </NuxtLink>
+                  </q-td>
+                  <q-td>
+                    {{ props.row.isCompleted }}
+                  </q-td>
+                </q-tr>
+              </template>
+            </q-table>
           </div>
         </template>
       </template>
@@ -51,6 +69,9 @@ import type {
 
 let countries = ref([] as Array<Countries>);
 const loading = ref(false);
+const completedCountryCount = ref(0);
+const columns = ref([] as any);
+const rows = ref([] as any);
 
 const getAllCountries = async () => {
   loading.value = true;
@@ -80,9 +101,42 @@ const onClickCountryName = async (countryId: number) => {
   }
 };
 
-onMounted(() => {
-  getAllCountries();
+const countCompletedCountry = (countries: Array<Countries>) => {
+  return countries.filter((country: Countries) => country.completed).length;
+};
+
+onMounted(async () => {
+  await getAllCountries();
+  completedCountryCount.value = countCompletedCountry(countries.value);
+  getQTableValue();
 });
+
+const getQTableValue = () => {
+  columns.value = [
+    {
+      name: 'countryName',
+      required: true,
+      label: '国名',
+      align: 'center',
+      field: (row: { name: any }) => row.name,
+      format: (val: any) => `${val}`,
+    },
+    {
+      name: 'isCompleted',
+      required: true,
+      label: '訪問経験',
+      align: 'center',
+      field: 'isCompleted',
+    },
+  ];
+  rows.value = countries.value.map((country) => {
+    return {
+      id: country.id,
+      name: country.name,
+      isCompleted: country.completed ? '訪問済み' : '未訪問',
+    };
+  });
+};
 </script>
 
 <style lang="css" scoped>
@@ -96,8 +150,21 @@ th {
   height: 70px;
 }
 
-.countryList {
+.country-list {
   text-align: center;
   display: inline-block;
+}
+
+.link {
+  text-decoration: none;
+  color: #3ea6ff;
+  display: inline-block;
+  transition: 0.3s;
+  transform: scale(1);
+}
+
+.link:hover {
+  font-weight: bold;
+  transform: scale(1.05);
 }
 </style>
